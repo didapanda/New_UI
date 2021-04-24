89a4e9e0e770dd04c0bd304c8334a1ed5b75573d
panostsokas <panagiotistsokas21@gmail.com>
2021-04-25 01:13:10 +0300

    datatable

---
+++ .history/pages/infos_20210424235305.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="page-parent">
+    <h1 class="page-title">{{ $t('infos') }}</h1>
+    <p class="page-subtitle pl-1">{{ $t('infos_txt') }}</p>
+    <img class="page-img" src="~assets/img/Group.png" />
+  </div>
+</template>
---
+++ .history/pages/infos_20210425005055.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="page-parent">
+    <h1 class="page-title ml-9">{{ $t('infos') }}</h1>
+    <p class="page-subtitle ml-9 pl-1">{{ $t('infos_txt') }}</p>
+  </div>
+</template>
