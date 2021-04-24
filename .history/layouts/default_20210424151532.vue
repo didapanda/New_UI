4e7559c8e8d1344b428748817373666d9ceb8334
panostsokas <panagiotistsokas21@gmail.com>
2021-04-24 17:15:50 +0300

    mobile view homepage

---
+++ .history/layouts/default_20210424151532.vue
@@ -0,0 +1,163 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      width="350"
+      fixed
+      app
+      :mini-variant="miniVariant"
+      :mini-variant-width="60"
+      :clipped="clipped"
+    >
+      <nuxt-link nuxt :to="localePath(`/`)">
+        <div v-if="!miniVariant" class="ml-12">
+          <img
+            class="img"
+            max-height="150"
+            max-width="250"
+            alt="Pandaswap"
+            src="~/assets/img/logo.svg"
+          />
+        </div>
+        <div v-if="miniVariant" class="ml-12">
+          <img
+            class="img"
+            max-height="150"
+            max-width="250"
+            alt="Pandaswap"
+            src="~/assets/img/Logo_panda.svg"
+          />
+        </div>
+      </nuxt-link>
+      <v-list class="pt-16 mt-16 ml-12 mr-8" nav dense rounded>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i + 1"
+          class="item"
+          :to="localePath(item.to)"
+          router
+          exact
+        >
+          <v-list-item-icon class="ml-12">
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <hr class="hr mx-4" />
+      <div class="pt-4 text-center">
+        <span class="text">{{ $t('bamboo_price') }}</span>
+        <br />
+        <span class="bamboo-value mr-16 pr-2">{{ bamboo_value }}</span>
+      </div>
+      <v-btn class="ml-16 button" outlined rounded color="black">
+        {{ $t('connect_wallet') }}
+      </v-btn>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
+        <Toast></Toast>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fab: false,
+      miniVariant: false,
+      clipped: false,
+      drawer: true,
+    }
+  },
+  computed: {
+    bamboo_value() {
+      return '$0.5'
+    },
+    items() {
+      return [
+        {
+          icon: 'mdi-home',
+          title: this.$t('home'),
+          to: '/',
+        },
+        {
+          icon: 'mdi-home',
+          title: this.$t('pools'),
+          to: '/pools',
+        },
+        {
+          icon: 'mdi-home',
+          title: this.$t('trade'),
+          to: '/trade',
+        },
+        {
+          icon: 'mdi-swap-horizontal-bold',
+          title: this.$t('farm'),
+          to: '/farm',
+        },
+        {
+          icon: 'mdi-information',
+          title: this.$t('infos'),
+          to: '/infos',
+        },
+      ]
+    },
+  },
+  methods: {
+    goto() {
+      this.$router.push('/content/companies')
+    },
+    name() {
+      return 'name'
+    },
+  },
+}
+</script>
+
+<style scoped>
+.icon {
+  height: 40px;
+  width: 40px;
+}
+.sidebar {
+  height: 100vh;
+}
+.img {
+  margin-top: 40px;
+  margin-left: -20px !important;
+  z-index: 900 !important;
+}
+/* .item {
+  height: 60px;
+  max-width: 224px;
+} */
+.hr {
+  max-width: 320px;
+}
+.text {
+  color: #00000059;
+  font-weight: 700;
+  font-size: 14px;
+}
+.bamboo-value {
+  font-weight: 700;
+  font-size: 18px;
+}
+.button {
+  margin-left: 84px;
+  position: absolute;
+  bottom: 46px;
+}
+.background {
+  width: 100vw;
+  height: 100vh;
+  background: url('~assets/img/typebg.svg') no-repeat center;
+}
+</style>
---
+++ .history/pages/index_20210424165431.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div v-if="!mini" class="background">
+      <div class="parent">
+        <p class="big-text">{{ $t('panda_swap') }}</p>
+        <p class="small-text pl-3">{{ $t('home_text') }}</p>
+      </div>
+      <div class="footer">
+        {{ $t('powered_by') }}
+        <img class="pl-4" src="~assets/img/avaxlogo.png" />
+      </div>
+    </div>
+    <div v-if="mini">
+      <div class="parent-mini">
+        <p class="big-text-mini">{{ $t('panda_swap') }}</p>
+        <p class="small-text-mini">{{ $t('home_text') }}</p>
+      </div>
+      <div class="text-xs-center">
+        <v-btn class="button" outlined rounded color="black">
+          {{ $t('connect_wallet') }}
+        </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['mini']),
+  },
+}
+</script>
+<style scoped>
+.background {
+  width: 100vw;
+  height: 100vh;
+  left: -94.35px;
+  top: -2px;
+  background: url('~assets/img/bubble_with_bg.svg') no-repeat right;
+}
+.parent {
+  display: inline-block;
+  width: 500px;
+  height: auto;
+  margin: auto;
+  padding: 10px;
+  position: relative;
+  top: 30%;
+  left: 5%;
+}
+.big-text {
+  font-size: 72px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 108px;
+  letter-spacing: 0em;
+  text-align: center;
+}
+.small-text {
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #168e80;
+}
+.footer {
+  display: inline-block;
+  /* width: 500px; Can be in percentage also. */
+  height: auto;
+  margin: auto;
+  position: relative;
+  top: 50%;
+  right: 20%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 27px;
+  letter-spacing: 0.2em;
+  text-align: left;
+  color: #00000059;
+}
+.parent-mini {
+  color: #335d2d;
+  text-align: center;
+}
+.big-text-mini {
+  font-style: normal;
+  font-size: 48px;
+  font-weight: 700;
+}
+</style>
